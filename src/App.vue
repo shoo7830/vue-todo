fe3f0f36e7099d0a45501b72f99855001ed76947
GW1402535\GW1402535 <shoo7830@gmail.com>
2018-03-16 10:02:46 +0900

    do it vue.js 완독!

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -15,6 +15,36 @@ import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+
+  methods: {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+
   components: {
     "TodoHeader": TodoHeader,
     "TodoInput": TodoInput,
--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>footer</div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearTodo() {
+      this.$emit('removeAll');
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background: #fff;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
 
+  .clearAllBtn {
+    color: #e23030;
+    display: block;
+  }
 </style>
